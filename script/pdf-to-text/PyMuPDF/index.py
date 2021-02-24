98dec2ffa4bab383c3d100daaa4568e6b71ea9a2
aabccd021 <aabccd021@gmail.com>
2021-02-24 17:40:08 +0700

    fix order of blocks

--- script/pdf-to-text/PyMuPDF/index.py
+++ script/pdf-to-text/PyMuPDF/index.py
@@ -1,12 +1,14 @@
 import sys
 import fitz
 import os
+import json
+
 if __name__ == "__main__":
     pdf_path = sys.argv[1]
     text_path = sys.argv[2]
     pages = fitz.Document(pdf_path)
-    pageTexts = [page.getText() for page in pages]
-    lines = [line for line in pageTexts]
-    text = '\n'.join(lines)
+    pageDicts = [page.getText('json') for page in pages]
+    text = ',\n'.join(pageDicts)
+    text = f'[{text}]'
     with open(text_path, 'w') as f:
         f.write(text)
--- src/command/convert/pdf-to-text/index.ts
+++ src/command/convert/pdf-to-text/index.ts
@@ -1,19 +1,23 @@
-import { chain, isEmpty } from 'lodash';
 import { execSync } from 'child_process';
 import * as fs from 'fs';
+import { chain, curry, isEmpty } from 'lodash';
 import { getDocumentData, getDocumentFilePath } from '../../../data';
 import { DocumentNode } from '../../../legal/document';
 
 type Option = { overwrite: boolean };
 export function pdfToTxt(option: Option): void {
   const pdfs = getDocumentData('pdf');
-  pdfs.forEach((pdf) => handlePdf(pdf, option));
+  pdfs.forEach(handlePdf(option));
 }
 
-function handlePdf(pdf: DocumentNode, option: Option): void {
+const handlePdf = curry(_handlePdf);
+
+function _handlePdf(option: Option, pdf: DocumentNode): void {
   const { overwrite } = option;
   const pdfFile = getDocumentFilePath(pdf, 'pdf');
   const { path: textPath, exists: textExists } = getDocumentFilePath(pdf, 'text');
+
+  console.log(`Start processing ${textPath}`);
   try {
     if (!overwrite && textExists) {
       console.log(`Skipped pdf-to-text ${textPath}`);
@@ -27,9 +31,10 @@ function handlePdf(pdf: DocumentNode, option: Option): void {
     fs.writeFileSync(textPath, processedText.join('\n'));
 
     console.log(`Finished pdf-to-text ${textPath}`);
-  } catch {
-    console.log(`Error pdf-to-text ${textPath}`);
+  } catch (e) {
+    console.error(`Error pdf-to-text ${textPath}: ${e}`);
   }
+  console.log();
 }
 
 function postProcess(lines: string[]): string[] {
@@ -46,4 +51,48 @@ function removePageNoise(lines: string[], line: string): string[] {
 
 function PyMuPDF(pdfPath: string, textPath: string): void {
   execSync(`/usr/bin/python3 script/pdf-to-text/PyMuPDF/index.py ${pdfPath} ${textPath}`);
+  const rawText = fs.readFileSync(textPath).toString();
+  const extraction: PyMuPDFPage[] = JSON.parse(rawText);
+  const text = extraction
+    .flatMap(({ blocks }) => chain(blocks).compact().sort(byLineYCoordinate).value())
+    .flatMap((block) => block?.lines)
+    .flatMap((line) => line?.spans?.map((span) => span?.text).join(''))
+    .join('\n');
+  fs.writeFileSync(textPath, text);
 }
+
+function byLineYCoordinate(a: Block, b: Block): number {
+  return a.bbox[3] - b.bbox[3];
+}
+
+type Bbox = [number, number, number, number];
+
+type Span = {
+  size?: number;
+  flags?: number;
+  font?: string;
+  color?: number;
+  text?: string;
+  origin?: [number, number];
+  bbox?: number[];
+};
+
+type Line = Partial<{
+  spans: Span[];
+  wmode: number;
+  dir: number[];
+  bbox: number[];
+}>;
+
+type Block = {
+  number?: number;
+  type?: number;
+  bbox: Bbox;
+  lines?: Line[];
+};
+
+type PyMuPDFPage = Partial<{
+  width: number;
+  height: number;
+  blocks: Block[];
+}>;
