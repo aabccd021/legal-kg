0f9d8506d3fa72d02113172c88e882eebabe2309
aabccd021 <aabccd021@gmail.com>
2021-02-24 13:24:05 +0700

    tried to support new document

--- src/command/convert/pdf-to-text/index.ts
+++ src/command/convert/pdf-to-text/index.ts
@@ -1,4 +1,4 @@
-import { isEmpty } from 'lodash';
+import { chain, isEmpty } from 'lodash';
 import { execSync } from 'child_process';
 import * as fs from 'fs';
 import { getDocumentData, getDocumentFilePath } from '../../../data';
@@ -23,8 +23,8 @@ function handlePdf(pdf: DocumentNode, option: Option): void {
     PyMuPDF(pdfFile.path, textPath);
 
     const rawText = fs.readFileSync(textPath).toString();
-    const processedText = postProcess(rawText);
-    fs.writeFileSync(textPath, processedText);
+    const processedText = postProcess(rawText.split('\n'));
+    fs.writeFileSync(textPath, processedText.join('\n'));
 
     console.log(`Finished pdf-to-text ${textPath}`);
   } catch {
@@ -32,13 +32,25 @@ function handlePdf(pdf: DocumentNode, option: Option): void {
   }
 }
 
-function postProcess(raw: string): string {
-  const lines = raw.split('\n');
-  return lines
+function postProcess(lines: string[]): string[] {
+  return chain(lines)
     .map((line) => line.trim())
     .filter((line) => !isEmpty(line))
-    .join('\n');
+    .reduce(removePageNoise, { removeCount: 0, lines: [] })
+    .thru(({ lines }) => lines)
+    .value();
 }
+
+function removePageNoise(acc: RemovePageAccumulator, line: string): RemovePageAccumulator {
+  const { lines } = acc;
+  const newLines = [...lines, line];
+  return { ...acc, lines: newLines };
+}
+
+type RemovePageAccumulator = {
+  removeCount: number;
+  lines: string[];
+};
 
 function PyMuPDF(pdfPath: string, textPath: string): void {
   execSync(`/usr/bin/python3 script/pdf-to-text/PyMuPDF/index.py ${pdfPath} ${textPath}`);
