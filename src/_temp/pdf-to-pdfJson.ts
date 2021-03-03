ae334a21386b6e2e8760cdf4b148ce0a2414f779
aabccd021 <aabccd021@gmail.com>
2021-03-03 14:54:05 +0700

    remove header from all

--- src/_temp/pdf-to-pdfJson.ts
+++ src/_temp/pdf-to-pdfJson.ts
@@ -1,23 +1,28 @@
+import { DocumentNode } from './../legal/document/index';
 import { writeFileSync } from 'fs';
 import { PDFExtract, PDFExtractPage, PDFExtractText } from 'pdf.js-extract';
+import { getDocumentData, getDocumentFilePath } from '../data';
 
 const pdfExtract = new PDFExtract();
 
 async function pdfToPdfJson(): Promise<void> {
-  const { pages } = await pdfExtract.extract('maintained_documents/normalized-pdf/uu/2003/13.pdf');
-  // writeFileSync('pagesx.json', JSON.stringify(pages, undefined, 2));
-  // writeFileSync('pagesx.text', textOf(pages));
+  for (const pdf of getDocumentData('normalized-pdf')) {
+    await toPdfJson(pdf);
+  }
+}
+
+async function toPdfJson(pdfNode: DocumentNode): Promise<void> {
+  console.log('start', pdfNode);
+  const pdfFile = getDocumentFilePath(pdfNode, 'normalized-pdf');
+  const jsonFile = getDocumentFilePath(pdfNode, 'pdf-data');
+  const { pages } = await pdfExtract.extract(pdfFile.path);
   const cleanPages = pages.map(toPageWithoutNoise);
-  writeFileSync(
-    'maintained_documents/pdf-data/uu/2003/13.json',
-    JSON.stringify(cleanPages, undefined, 2)
-  );
-  // writeFileSync('clean_pagesx.text', textOf(cleanPages));
+  writeFileSync(jsonFile.path, JSON.stringify(cleanPages, undefined, 2));
 }
 
 function toPageWithoutNoise(page: PDFExtractPage, _pageIdx: number): PDFExtractPage {
   const { content } = page;
-  // console.log('\n===PAGE', _pageIdx + 1);
+  console.log('\n===PAGE', _pageIdx + 1);
   const newContent = content.filter(isNotPageNoise);
   return { ...page, content: newContent };
 }
