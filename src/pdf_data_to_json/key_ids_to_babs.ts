921ea8c1f70cdd037676fbee132c376145b51ab3
aabccd021 <aabccd021@gmail.com>
2021-03-08 21:45:44 +0700

    checkpoint

--- src/pdf_data_to_json/key_ids_to_babs.ts
+++ src/pdf_data_to_json/key_ids_to_babs.ts
@@ -1,27 +1,48 @@
-// import { curry } from 'lodash';
-// import { Bab } from './../legal/structure/bab';
-// import { Bab } from '../legal/structure/bab';
-// import { lastOf, Span } from '../util';
-// import { BabKeySpanIds } from './babs_spans_to_key_ids';
+import { chain, curry, isUndefined } from 'lodash';
+import { Span } from '../util';
+import { BabKeySpanIds } from './babs_spans_to_key_ids';
 
-// export function keyIdsToBabs(spanKeyIds: BabKeySpanIds, spans: Span[]): Bab[] {
-//   const initial: Acc = { keySpans: []};
-//   const a = spans.reduce(toBabsSpansWith(spanKeyIds), initial);
-// }
+export function babsOfKeyIds(spanKeyIds: BabKeySpanIds, spans: Span[]): SpansOfKey {
+  const initial: Acc = { spansOfKey: {} };
+  return chain(spans)
+    .reduce(toBabsSpansWith(spanKeyIds), initial)
+    .thru(({ spansOfKey }) => spansOfKey)
+    .value();
+  // .toPairs();
+}
 
-// type Acc = {
-// keySpans: {[key: number]: Span[]}
-// currentKey?: number;
-// };
+type Acc = {
+  spansOfKey: SpansOfKey;
+  currentKey?: number;
+};
 
-// const toBabsSpansWith = curry(toBabsSpans);
+type SpansOfKey = { [key: number]: Span[] };
 
-// export function toBabsSpans(spanKeyIds: BabKeySpanIds, babSpans: Acc, span: Span, ): Acc {
-//   const
-//   const newBabSpans: BabSpans[] = [...]
-//   return [];
-// }
+const toBabsSpansWith = curry(toBabsSpans);
 
-// function babOfSpans(spans: Span[]): Bab[] {
+export function toBabsSpans(keySpanIds: BabKeySpanIds, acc: Acc, span: Span): Acc {
+  const { spansOfKey, currentKey } = acc;
+  const { babKeyOfid } = keySpanIds;
+  const newKey = babKeyOfid[span.id];
 
-// }
+  if (isUndefined(currentKey)) {
+    if (newKey !== 1) throw Error('impossible');
+    // first
+    const newSpansOfKey = { [newKey]: [] };
+    return { ...acc, currentKey: newKey, spansOfKey: newSpansOfKey };
+  }
+
+  if (!isUndefined(newKey)) {
+    const newSpansOfKey = { ...spansOfKey, [newKey]: [] };
+    return { ...acc, currentKey: newKey, spansOfKey: newSpansOfKey };
+  }
+
+  const currentSpans = spansOfKey[currentKey];
+  if (isUndefined(currentSpans)) throw Error(JSON.stringify(acc));
+
+  const newCurrentSpans = [...currentSpans, span];
+  const newSpansOfKey = { ...spansOfKey, [currentKey]: newCurrentSpans };
+  return { ...acc, spansOfKey: newSpansOfKey };
+}
+
+// function babOfSpans(spans: Span[]): Bab[] {}
