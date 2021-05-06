797678dc8ef665a96528ff4192bd4860fe7c74d2
aabccd021 <aabccd021@gmail.com>
2021-05-06 07:15:48 +0700

    attempt 73

---
+++ src/ocr.ts
@@ -0,0 +1,32 @@
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import { getDocumentData, nodeToFile, shouldOverwrite } from './util';
+
+const awaitExec = promisify(exec);
+
+async function ocr(): Promise<void> {
+  const jsonNodes = getDocumentData('pdf');
+
+  jsonNodes.forEach((node) => {
+    console.log('\nstart', node);
+
+    const rawPdfFile = nodeToFile('pdf', node);
+    const normalizedPdfFile = nodeToFile('normalized-pdf', node);
+
+    if (!shouldOverwrite() && normalizedPdfFile.exists) {
+      console.log('skipped because exists');
+      return;
+    }
+
+    awaitExec(
+      `ocrmypdf` +
+        ` -l ind` +
+        ` --force-ocr` +
+        ` --jobs 4` +
+        ` --tesseract-config tesseract-config.cfg` +
+        ` ${rawPdfFile.path} ${normalizedPdfFile.path}`
+    );
+  });
+}
+
+ocr();
