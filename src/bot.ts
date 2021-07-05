e2da85369fa1889023a66a1e1e4178643ee3ae39
aabccd021 <aabccd021@gmail.com>
2021-07-05 14:46:57 +0700

    bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,35 +1,54 @@
 import * as readline from 'readline';
+import { sparqlQuery } from './sparql';
+
+type Context = { type: 'init' | 'search' | 'searchResult'; text: string };
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-type Context = { type: 'init'; text: string } | { type: 'search'; text: string };
-
-const initHelp = `Ada yang bisa dibantu?
-1. Mencari Peraturan Perundang-undangan`;
-
 let context: Context = {
   type: 'init',
-  text: initHelp,
+  text: `Ada yang bisa dibantu?
+1. Mencari Peraturan Perundang-undangan`,
 };
 
 console.log(context.text);
 
-rl.on('line', function (line) {
-  context = reducer(line, context);
-  console.log(context.text);
+rl.on('line', async function (line) {
+  context = await reducer(line, context);
+  process.stdout.write(context.text);
+  if (context.type === 'searchResult') {
+    process.exit(0);
+  }
 });
 
-function reducer(input: string, context: Context): Context {
+async function reducer(inputStr: string, context: Context): Promise<Context> {
   if (context.type === 'init') {
-    if (input === '1') {
-      return { type: 'search', text: 'Masukkan Keyword Peraturan' };
+    if (inputStr === '1') {
+      return { type: 'search', text: 'Masukkan Keyword Peraturan: ' };
     }
   }
-	if (context.type === 'search') {
+  if (context.type === 'search') {
+    const res = await sparqlQuery({
+      queryStr: `
+PREFIX lex2kg-o: <https://example.org/lex2kg/ontology/>
 
-	}
+SELECT ?doc ?title
+WHERE {
+  ?doc a lex2kg-o:Peraturan .
+  ?doc lex2kg-o:tentang ?title.
+  FILTER REGEX(STR(?title), "${inputStr.toUpperCase()}")
+}
+`,
+    });
+    return {
+      type: 'searchResult',
+      text:
+        `Berikut adalah peraturan yang judulnya mengandung ${inputStr}` +
+        res.map((row) => row.doc?.value.replace('https://example.org/lex2kg', '')).join('\n'),
+    };
+  }
   return { ...context, text: `Input tidak diketahui\n${context.text}` };
 }
